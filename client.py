119f363f28ab510bc6c2300a0c801b16b5a85f6b
Adam Thieme <adam.thieme@gmail.com>
2022-04-18 02:04:23 -0500

    using yummy as parent for client object

--- client.py
+++ client.py
@@ -8,5 +8,41 @@ def start():
     send(".exit", client)
 
 
+class Client(Yummy):
+    def __init__(self):
+        super().__init__()
+
+    def callback(self, conn, message, addr=None):
+        if message == DISCONNECT_MESSAGE:
+            print('You have disconnected. Please press ENTER to finish.')
+            return False
+        print(f'{message}')
+        return True
+
+    def handle_connection(self, conn, addr=None):
+        while True:
+            try:
+                message = receive(conn)
+            except ConnectionResetError:
+                break
+            if message:
+                if not self.callback(conn, message):
+                    break
+        exit()
+
+    def listen(self):
+        self.sock.connect(ADDR)
+        t = threading.Thread(target=self.handle_connection, args=[self.sock])
+        t.start()
+        while True:
+            try:
+                out = input('')
+                self.send(out)
+            except ConnectionAbortedError:
+                break
+            except ValueError:
+                break
+
+
 if __name__ == '__main__':
-    start()
+    y = Client()
