40d433f21bdd572b09ce6c1a31b9ec0167c85098
Adam Thieme <adam.thieme@gmail.com>
2022-04-18 02:52:25 -0500

    uses yummy and adds connection things

--- server.py
+++ server.py
@@ -1,45 +1,81 @@
 import select
 import socket
 import threading
-from common import *
+import time
 
+from random import randint
+
+from common import *
 
 SOCKET_LIST = []
 
-
-# Handles all communication between server and client
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTIONS] {addr} connected.")
-
-    connected = True
-    while connected:
-        response = receive(conn)
-        if response:
-            if response == DISCONNECT_MESSAGE:
-                connected = False
-            print(response)
-
-    conn.close()
+conns_dict = {}
 
 
-# Starts the server socket and listens for connections and handles the connections and then passing them to
-# handle_client()
-def start():
-    # This will generate a socket that will the server to connect to the client(s)
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Server(Yummy):
+    def __init__(self):
+        super().__init__()
 
-    # Binds the socket to the server address
-    server.bind(ADDR)
-    SOCKET_LIST.append(server)
+    def listen(self):
+        self.sock.bind(ADDR)
+        self.sock.listen()
+        while True:
+            conn, addr = self.sock.accept()
+            thread = threading.Thread(target=self.handle_connection, args=(conn, addr))
+            thread.start()
 
-    server.listen()
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+    def create_id(self):
+        r = -1
+        x = 2
+        while r < 1 or str(r) in conns_dict:
+            r = randint(0, x)
+            x *= 2
+        return str(r)
+
+    def send_list(self):
+        for person in conns_dict:
+            tmp = ''
+            for key in conns_dict:
+                if key != person:
+                    tmp += str(key) + '\n'
+            if tmp != '':
+                self.send(f'Available connections as IDs:\n{tmp}', conns_dict[person])
+
+    def handle_connection(self, conn, addr=None):
+        print(f"[NEW CONNECTIONS] {addr} connected.")
+        conn_id = self.create_id()
+        conns_dict[conn_id] = conn
+        print(f'[ID GENERATED]: {conn_id} for {addr}')
+        self.send(f'Your ID is {conn_id}.', conn)
+        self.send_list()
+        talking_to = []
+        while True:
+            try:
+                message = receive(conn)
+            except ConnectionResetError:
+                break
+            if message:
+                if message == DISCONNECT_MESSAGE:
+                    try:
+                        send(DISCONNECT_MESSAGE, conn)
+                        conn.close()
+                    except:
+                        'placeholder'
+                    break
+                if len(talking_to) == 0:
+                    if message in conns_dict:
+                        talking_to.append(conns_dict[message])
+                    else:
+                        if message == 'help':
+                            self.send(DISCONNECT_MESSAGE, conn)
+                        else:
+                            self.send(message, conn)
+                else:
+                    for person in talking_to:
+                        self.send(message, person)
 
 
 if __name__ == '__main__':
     print("Welcome! Server is starting now!")
-    start()
+    # start()
+    Server()
